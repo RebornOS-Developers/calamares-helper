939f437f23c794c11de92045bf07c58fca35714d
shivanandvp <shivanand.pattanshetti@gmail.com>
2022-01-02 18:25:07 -0500

    Naming changes

--- setup_all.sh
+++ setup_all.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIRECTORY="$(dirname -- "$(readlink -f -- "$0")")"
 
 set -o xtrace
-sh "$SCRIPT_DIRECTORY"/build_all_packages.sh --install "$@"
+sh "$SCRIPT_DIRECTORY"/build_all.sh --install "$@"
 set +o xtrace
