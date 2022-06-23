c1c9895756cc9a11876ba3f031a998de8c1d43a7
shivanandvp <shivanandvp.oss@gmail.com>
2022-06-23 04:51:40 -0500

    feat: Updater script

---
+++ update.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env sh
+
+# Checkout a branch for the superproject and all constituent subprojects together so that they do not go out of sync
+# The branch name is provided as an argument to this script
+
+SCRIPT_DIRECTORY="$(dirname -- "$(readlink -f -- "$0")")"
+PROJECT_DIRECTORY="$(dirname -- "$SCRIPT_DIRECTORY")"
+
+CONFIGURATION_DIRECTORY="$PROJECT_DIRECTORY/calamares-configuration"
+CORE_DIRECTORY="$PROJECT_DIRECTORY/calamares-core"
+
+(cd "$CONFIGURATION_DIRECTORY" && git pull "$@") && \
+(cd "$CORE_DIRECTORY"          && git pull "$@") && \
+(cd "$SCRIPT_DIRECTORY"        && git pull "$@")
